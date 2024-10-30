cc7f6355931c1f3dfd083b136e24b7850175b43b
joseca20 <crisskcres1720@gmail.com>
2024-10-30 16:14:43 -0500

    Se crea proyecto en spring boot

---
+++ src/test/java/com/jeictechnology/literAlura/LiterAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeictechnology.literAlura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
